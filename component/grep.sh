89f840c23b156e929798f2b42211f9984eb67a76
c332030 <c332030@gmail.com>
2022-01-07 18:37:12 +0800

    feat: grep

--- component/grep.sh
+++ component/grep.sh
@@ -2,3 +2,11 @@
 # grep 查找文件内容
 
 grep -r 'content' /
+
+# 上下文
+grep -C 5 foo file  显示file文件中匹配foo字串那行以及上下5行
+grep -B 5 foo file  显示foo及前5行
+grep -A 5 foo file  显示foo及后5行
+
+# 去重
+grep -A 10 "Exception: " logback/error.log | uniq -c > exception.log
