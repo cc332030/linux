d27cca79a9ab1ebe5c2dda9eefc68ac304acd6e0
c332030 <c332030@gmail.com>
2022-07-11 21:38:35 +0800

    feat: USER_HOME

--- script/init-gpg/init-gpg.sh
+++ script/init-gpg/init-gpg.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
-set -e
+echo 'init-gpg'
 
-export GPG_TTY="--pinentry-mode loopback"
+set -e
 
 if [ ! "$GPG_PRIVATE_KEY" ]; then
   echo GPG_PRIVATE_KEY not exists
   exit 1
 fi
 
-GPG_FOLDER=~/.gnupg
+export GPG_TTY="--pinentry-mode loopback"
 
-mkdir -p $GPG_FOLDER
-chmod -R 700 $GPG_FOLDER
+user=$(whoami)
 
-echo "allow-loopback-pinentry" >> "${GPG_FOLDER}/gpg-agent.conf"
+echo "user: $user"
+echo "home:" ~
+
+if [ "root" = "$user" ]
+then
+  USER_HOME=/root
+else
+  USER_HOME=/home/$user
+fi
+
+GPG_PATH=$USER_HOME/.gnupg
+echo "GPG_PATH: $GPG_PATH"
+
+mkdir -p "$GPG_PATH"
+chmod -R 700 "$GPG_PATH"
+
+echo "allow-loopback-pinentry" >> "${GPG_PATH}/gpg-agent.conf"
 
 echo "$GPG_PRIVATE_KEY" | gpg --batch --import
 
 echo "$GPG_PASSWORD" | \
   gpg $GPG_TTY \
   --passphrase-fd 0  \
-  --export-secret-keys -o "${GPG_FOLDER}/secring.gpg"
+  --export-secret-keys -o "${GPG_PATH}/secring.gpg"
+
+echo 'init-gpg successfully'
