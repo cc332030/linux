8fb4ea661814cf41220c812485ead260651aaf49
c332030 <c332030@gmail.com>
2022-07-19 11:14:11 +0800

    feat: update dns

--- script/init-ssh/init-ssh-pre.sh
+++ script/init-ssh/init-ssh-pre.sh
@@ -7,4 +7,11 @@ set -e
 mkdir -p /etc/ssh
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
+cat /etc/resolv.conf
+
+echo "search com
+nameserver 8.8.8.8
+nameserver 1.1.1.1
+" > /etc/resolv.conf
+
 echo 'init-ssh-pre successfully'
