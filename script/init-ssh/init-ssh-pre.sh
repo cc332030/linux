d35d567468219a5e803023fbd46c7a9e12753571
c332030 <c332030@gmail.com>
2022-07-19 11:48:46 +0800

    feat: remove debug log

--- script/init-ssh/init-ssh-pre.sh
+++ script/init-ssh/init-ssh-pre.sh
@@ -12,10 +12,6 @@ echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
 # 避免 gitee 超时
 GITEE_COM=gitee.com
-
-dig @8.8.8.8 $GITEE_COM
-nslookup $GITEE_COM 8.8.8.8
-
 GITEE_IP=$(nslookup $GITEE_COM 8.8.8.8 |
              grep "Address: " |
              awk '{print $2}')
