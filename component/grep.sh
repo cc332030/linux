73f04494e9c48b72863cb27f24aae756827cf9f0
c332030 <c332030@gmail.com>
2022-08-24 11:49:57 +0800

    feat: grep 模糊查询

--- component/grep.sh
+++ component/grep.sh
@@ -13,3 +13,9 @@ grep -A 10 "java.lang.NullPointerException" logback/error.log | uniq -c > except
 
 # 去重 计数
 grep "Exception: " logback/error.log | sort | uniq -c
+
+# 模糊查询 .代表任意字符，*代表次数
+grep "LogAspect start: .*, cost: " log4j/debug.log
+
+# {min,max}，min 最小出现次数，max 最大出现次数，'{'和'}'需要转义
+grep "LogAspect start: .*, cost: [0-9]\{4,\}" log4j/debug.log
