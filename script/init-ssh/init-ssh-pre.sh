1ee7b558eacef1bc6a09343debda07ba19950f08
c332030 <c332030@gmail.com>
2022-09-04 15:31:15 +0800

    feat: remove write hosts

--- script/init-ssh/init-ssh-pre.sh
+++ script/init-ssh/init-ssh-pre.sh
@@ -8,13 +8,4 @@ set -e
 mkdir -p /etc/ssh
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
-# 避免 gitee 超时
-GITEE_COM=gitee.com
-GITEE_IP=$(nslookup "$GITEE_COM" 8.8.8.8 |
-             grep "Address: " |
-             cut -d ' ' -f 2)
-
-echo "GITEE_IP: $GITEE_IP"
-echo "$GITEE_IP $GITEE_COM" >> /etc/hosts
-
 echo 'init-ssh-pre successfully'
