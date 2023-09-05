23f5d540ba3936e5dda5cf8f3f51523e3d1dc527
c332030 <c332030@c332030.com>
2023-09-05 17:22:21 +0800

    feat: init-ssh.sh

--- script/init-ssh/init-ssh.sh
+++ script/init-ssh/init-ssh.sh
@@ -18,27 +18,23 @@ chmod -R 700 "$WORK_PATH"
 echo "
 Host *
     ConnectTimeout 10
-" >> "$WORK_PATH/config"
+" >>"$WORK_PATH/config"
 chmod 600 "$WORK_PATH/config"
 
 ID_RSA_PATH="$WORK_PATH/id_rsa"
 
-echo "$SSH_PRIVATE_KEY" > "$ID_RSA_PATH"
+echo "$SSH_PRIVATE_KEY" >"$ID_RSA_PATH"
 chmod 600 "$ID_RSA_PATH"
 
-
-
 USER=$(whoami)
-if [ "root" = "$USER" ]
-then
+if [ "root" = "$USER" ]; then
   USER_HOME=/root
 else
   USER_HOME=/home/$USER
 fi
 echo "USER_HOME: ${USER_HOME}"
 
-if [ ! ~ = "$USER_HOME" ]
-then
+if [ ! ~ = "$USER_HOME" ]; then
   ln -s $WORK_PATH "$USER_HOME"
 fi
 
