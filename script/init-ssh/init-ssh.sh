5ef272b554ffae0c1601fad08460dada6e5f06b0
c332030 <c332030@c332030.com>
2025-09-21 14:42:07 +0800

    feat: init-ssh

--- script/init-ssh/init-ssh.sh
+++ script/init-ssh/init-ssh.sh
@@ -4,7 +4,7 @@ echo 'init-ssh'
 
 set -e
 
-if [ ! "$SSH_PRIVATE_KEY" ]; then
+if [ ! "${SSH_PRIVATE_KEY}" ]; then
   echo SSH_PRIVATE_KEY not exists
   exit 1
 fi
@@ -12,30 +12,30 @@ fi
 WORK_PATH=~/.ssh
 echo "WORK_PATH: ${WORK_PATH}"
 
-mkdir -p "$WORK_PATH"
-chmod -R 700 "$WORK_PATH"
+mkdir -p "${WORK_PATH}"
+chmod -R 700 "${WORK_PATH}"
 
 echo "
 Host *
     ConnectTimeout 10
-" >>"$WORK_PATH/config"
-chmod 600 "$WORK_PATH/config"
+" >>"${WORK_PATH}/config"
+chmod 600 "${WORK_PATH}/config"
 
-ID_RSA_PATH="$WORK_PATH/id_rsa"
+ID_RSA_PATH="${WORK_PATH}/id_rsa"
 
-echo "$SSH_PRIVATE_KEY" >"$ID_RSA_PATH"
-chmod 600 "$ID_RSA_PATH"
+echo "${SSH_PRIVATE_KEY}" >"${ID_RSA_PATH}"
+chmod 600 "${ID_RSA_PATH}"
 
 USER=$(whoami)
-if [ "root" = "$USER" ]; then
+if [ "root" = "${USER}" ]; then
   USER_HOME=/root
 else
-  USER_HOME=/home/$USER
+  USER_HOME=/home/${USER}
 fi
 echo "USER_HOME: ${USER_HOME}"
 
-if [ ! ~ = "$USER_HOME" ]; then
-  ln -s $WORK_PATH "$USER_HOME"
+if [ ! ~ = "${USER_HOME}" ]; then
+  ln -s ${WORK_PATH} "${USER_HOME}"
 fi
 
 echo 'init-ssh successfully'
